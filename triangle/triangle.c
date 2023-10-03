6f21f5c0acdb27397d5a08ca8a116c3cf60f64fd
robertssekisonge <133739996+robertssekisonge@users.noreply.github.com>
2023-10-03 13:19:03 -0700

    Add files via upload

---
+++ triangle/triangle.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    // Output an oval shape
+    printf("\t *****\n");
+    printf("\t*     *\n");
+    printf("\t*     *\n");
+    printf("\t *****\n\n");
+
+    // Output a triangle
+    printf("\t   /\\\n");
+    printf("\t  /  \\\n");
+    printf("\t /    \\\n");
+    printf("\t/______\\\n\n");
+
+    // Output a rectangle
+    printf("\t ______\n");
+    printf("\t|      |\n");
+    printf("\t|      |\n");
+    printf("\t|______|\n");
+
+    return 0;
+}
